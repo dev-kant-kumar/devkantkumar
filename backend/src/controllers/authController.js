62ca374ce84ef577c9684dab19d9aa40f136bf61
dev-kant-kumar <dev.mytechhandle@gmail.com>
2025-12-10 07:09:44 +0530

    fix cookie samesite policy for cross-subdomain

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -15,7 +15,7 @@ const sendTokenResponse = (user, statusCode, res, message = 'Success') => {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict'
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax'
   };
 
   res
